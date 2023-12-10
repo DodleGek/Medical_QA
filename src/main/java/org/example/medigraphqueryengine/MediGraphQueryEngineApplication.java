a154f9aff3e910a7da6dcbf72e6b329b63cea74b
DodleGek <2982780802@qq.com>
2023-12-10 19:57:36 +0800

    Initial commit

---
+++ src/main/java/org/example/medigraphqueryengine/MediGraphQueryEngineApplication.java
@@ -0,0 +1,13 @@
+package org.example.medigraphqueryengine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediGraphQueryEngineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MediGraphQueryEngineApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/medigraphqueryengine/MediGraphQueryEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.medigraphqueryengine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediGraphQueryEngineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
