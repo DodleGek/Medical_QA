6f8286c93aa2c3a6d416b8ef285b67af837f0076
DodleGek <2982780802@qq.com>
2023-12-27 09:46:29 +0800

    问题分类

---
+++ src/main/java/org/example/medigraphqueryengine/service/QuestionClassifier.java
@@ -0,0 +1,26 @@
+package org.example.medigraphqueryengine.service;
+
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class QuestionClassifier {
+    // 初始化方法，加载特征词，构建AC自动机，构建词类型字典，定义问题关键词等
+    public QuestionClassifier() {
+    }
+
+
+    // 分类方法，对输入的问题进行分类
+    public Object classify(String sent) {
+        return null;
+    }
+
+    // 构建词对应的类型方法
+
+    // 构建AC自动机方法
+
+    // 问题过滤方法
+
+    // 基于特征词进行分类方法
+}
