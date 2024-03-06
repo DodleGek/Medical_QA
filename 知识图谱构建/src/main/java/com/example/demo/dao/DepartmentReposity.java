7c85e31363479160eb1d20b32c6d71e859700c4d
李航宇 <13085566+lihangyuyu@user.noreply.gitee.com>
2024-03-06 08:05:21 +0000

    删除文件 知识图谱构建

--- 知识图谱构建/src/main/java/com/example/demo/dao/DepartmentReposity.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.dao;
-
-import com.example.demo.fact.Department;
-import org.springframework.data.neo4j.repository.Neo4jRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface DepartmentReposity extends Neo4jRepository<Department,Long> {
-}
--- 知识图谱构建/src/main/java/com/example/demo/dao/ProductorReposity.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.dao;
-
-import com.example.demo.fact.Productor;
-import org.springframework.data.neo4j.repository.Neo4jRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProductorReposity extends Neo4jRepository<Productor,Long> {
-}
