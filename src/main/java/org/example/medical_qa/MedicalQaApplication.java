6153a5c9260a3d566c31616bf3d5b83e7ff804ea
DodleGek <2982780802@qq.com>
2024-05-25 14:17:08 +0800

    Initial commit

---
+++ src/main/java/org/example/medical_qa/MedicalQaApplication.java
@@ -0,0 +1,13 @@
+package org.example.medical_qa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicalQaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedicalQaApplication.class, args);
+	}
+
+}
